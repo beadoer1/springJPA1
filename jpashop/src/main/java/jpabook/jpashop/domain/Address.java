1924954cbdbd4ff361d56088d03a89d2b97d6e71
beadoer1 <beadoer1@gmail.com>
2021-07-12 23:06:56 +0900

    Inflearn JPA 활용1 주문 화면 구성 commit 1

--- jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,12 +1,16 @@
 package jpabook.jpashop.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
 
     private String city;
